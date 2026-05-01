e1c98987ab7b28b3eb57ad31c56f7e567413e79d
bryan.salva <salvatorresb@gmail.com>
2026-05-01 18:58:43 -0500

    fix bugs and new conditions

--- easy_agents/tarea3_agente.py
+++ easy_agents/tarea3_agente.py
@@ -10,16 +10,16 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.logging_config import obtener_logger
 
 log = obtener_logger("Tarea3fundamentosIA")
-# 0 es para linea (No oscuro), 1 es linea(oscuro), -1 es para pared 
-N = 6
-M = 6
-ambiente = np.random.choice([0, 1], size=(N, M), p=[0.6, 0.4])
+# 0 es para linea (No oscuro), 1 es linea(oscuro), -1 es para pared (P)
+N = 5
+M = 5
+ambiente = np.random.choice([0, 1, -1], size=(N, M), p=[0.5, 0.4, 0.1])
 f  = np.random.randint(0, N-1)
 c  = np.random.randint(0, M-1)
 agent_pos = [0, 0]
 orientacion = 1 # Derecha
 pared = False
-max_pasos = 5
+max_pasos = 7
 
 print("AMBIENTE ORIGAL")
 print(ambiente)
@@ -48,9 +48,9 @@ def camara2(valor1, valor2, valor3):
     
     valores = {0:'NO OSCURO', 1: 'OSCURO', -1:'PARED'}
     
-    return {'PISO IZQ': valores[valor1],
-            'PISO CEN': valores[valor2],
-            'PISO DER': valores[valor3]
+    return {'AVANZAR CELDA IZQUIERDA': valores[valor1],
+            'AVANZAR CELDA CENTRAL': valores[valor2],
+            'AVANZAR CELDA DERECHA': valores[valor3]
         }
 
 def get_valor_seguro(matrix, i, j, valor_por_defecto=-1):
@@ -94,12 +94,12 @@ def posicion_celdas_delanteras(ambiente, orientacion, pos):
 
 def decidir_accion(percepcion_estados):
     
-    if percepcion_estados['PISO CEN'] == "OSCURO":     # prioridad 1 buscar por el cen
-        return "PISO CEN"
-    elif percepcion_estados['PISO DER'] == "OSCURO":   # prioridad 2 buscar por el der
-        return "PISO DER"
-    elif percepcion_estados['PISO IZQ'] == "OSCURO":   # prioridad 3 buscar por el izq
-        return "PISO IZQ"
+    if percepcion_estados['AVANZAR CELDA CENTRAL'] == "OSCURO":     # prioridad 1 buscar por el cen
+        return "AVANZAR CELDA CENTRAL"
+    elif percepcion_estados['AVANZAR CELDA DERECHA'] == "OSCURO":   # prioridad 2 buscar por el der
+        return "AVANZAR CELDA DERECHA"
+    elif percepcion_estados['AVANZAR CELDA IZQUIERDA'] == "OSCURO":   # prioridad 3 buscar por el izq
+        return "AVANZAR CELDA IZQUIERDA"
     else:
         return "ROTAR_-90"
 
@@ -107,7 +107,7 @@ def decidir_accion(percepcion_estados):
 # ===== BUCLE DE OPERACION ======
 #  0 es linea no oscura
 #  1 es linea oscura
-# -1 es pared
+# -1 es pared (P)
 
 for paso in range(max_pasos):
     f, c = agent_pos
@@ -116,6 +116,7 @@ for paso in range(max_pasos):
     valor_izq, valor_cen, valor_der = posicion_celdas_delanteras(ambiente, orientacion, agent_pos)
     estados_delanteros = camara2(valor_izq, valor_cen, valor_der)
     accion = decidir_accion(estados_delanteros)
+    
     # DIBUJAR EL ROBOT EN EL MAPA
     print(f"--- AMBIENTE {paso} ---")
     for row in range(N):
@@ -125,6 +126,8 @@ for paso in range(max_pasos):
                 fila_str = fila_str + f" {obtener_orientacion(orientacion)}  "
             elif ambiente[row, col] == 1:
                 fila_str = fila_str + " 1 "
+            elif ambiente[row, col] == -1:
+                fila_str = fila_str + " P "
             else:
                 fila_str = fila_str + " 0 "
             
@@ -133,6 +136,8 @@ for paso in range(max_pasos):
     print(f"valores delanteos:{valor_izq}, {valor_cen}, {valor_der}")
     print(f"Estado Actual: {estado_actual}")
     print(f"Estados delante: {estados_delanteros}")
+    print(f"Accion: {accion}")
+
     
     #print(ambiente)
 
@@ -140,8 +145,8 @@ for paso in range(max_pasos):
     pared = False
     if accion == "ROTAR_-90":
         orientacion = (orientacion + 1) % 4 # definicion de modulo
-    if accion == "PISO CEN":
-        estado_actual = estados_delanteros[accion]
+    if accion == "AVANZAR CELDA CENTRAL":
+        #estado_actual = estados_delanteros[accion]
         nueva_f, nueva_c = f, c
         if orientacion == 0:
             nueva_f = nueva_f - 1
@@ -150,6 +155,43 @@ for paso in range(max_pasos):
         elif orientacion == 2:
             nueva_f = nueva_f + 1
         elif orientacion == 3:
+            nueva_c = nueva_c - 1
+        if 0 <= nueva_f < N and 0 <= nueva_c < M and ambiente[nueva_f, nueva_c] != -1:
+            agent_pos = [nueva_f, nueva_c]
+        else:
+            pared = True
+    if accion == "AVANZAR CELDA IZQUIERDA":
+        #estado_actual = estados_delanteros[accion]
+        nueva_f, nueva_c = f, c
+        if orientacion == 0:
+            nueva_f = nueva_f - 1
+            nueva_c = nueva_c - 1
+        elif orientacion == 1:
+            nueva_f = nueva_f - 1
+            nueva_c = nueva_c + 1
+        elif orientacion == 2:
+            nueva_f = nueva_f + 1
+            nueva_c = nueva_c + 1
+        elif orientacion == 3:
+            nueva_f = nueva_f + 1
+            nueva_c = nueva_c - 1
+        if 0 <= nueva_f < N and 0 <= nueva_c < M and ambiente[nueva_f, nueva_c] != -1:
+            agent_pos = [nueva_f, nueva_c]
+        else:
+            pared = True
+    if accion == "AVANZAR CELDA DERECHA":
+        nueva_f, nueva_c = f, c
+        if orientacion == 0:
+            nueva_f = nueva_f - 1
+            nueva_c = nueva_c + 1
+        elif orientacion == 1:
+            nueva_f = nueva_f + 1
+            nueva_c = nueva_c + 1
+        elif orientacion == 2:
+            nueva_f = nueva_f + 1
+            nueva_c = nueva_c - 1
+        elif orientacion == 3:
+            nueva_f = nueva_f - 1
             nueva_c = nueva_c - 1
         if 0 <= nueva_f < N and 0 <= nueva_c < M and ambiente[nueva_f, nueva_c] != -1:
             agent_pos = [nueva_f, nueva_c]
